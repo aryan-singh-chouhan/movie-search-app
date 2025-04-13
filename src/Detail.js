e2accf9a0adcc91d3e4adcdad55d5d73a33604a8
Aryan Singh chouhan <aryanchouhan691@gmail.com>
2025-04-13 16:46:13 +0530

    Updated Detail.js and App.css; added Details.css

--- src/Detail.js
+++ src/Detail.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import './Details.css';
 import { useSelector } from 'react-redux';
 
 const Detail = () => {
-  const movie = useSelector((state)=>state.movie.movieDetail);
+  const movie = useSelector((state) => state.movie.movieDetail);
 
   return (
-    <div>{movie.Title}
-    <div>{movie.year}</div></div>
+    <div className='detailContainer'>
+      <div className='moviedetail'>
+
+        <div className='movieImage'>
+          <img
+            src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={"aaa"} />
+        </div>
+
+        <div className='movieDiscription'>
+          <p>Name: {movie.Title}</p>
+          <p>Type: {movie.Type}</p>
+          <p>Year: {movie.Year}</p>
+          <p>
+            <a
+              href={`https://hindilinks4u.horse/?s=${movie.Title?.split(' ').join('+')}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#f9d3b4", textDecoration: "underline" }}
+            >
+             Click Here to Watch: <br/>{movie.Title}
+            </a>
+          </p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
